9c2d2b50105da530b474cb185ad1096aaed493f8
mcflurrywhaat <chloe.v201@gmail.com>
2025-07-03 14:42:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.get("/contact", (req, res) => {
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
+app.get("/CODE-camp-test", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/", (req, res) => {
+app.get("/CODE-camp-test", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
